db3b6ca5262e84bb766aa60b6572e562c884c982
Richard Taylor <rjtaylor@qinetiq.com>
2014-10-07 10:35:23 +0100

    emacs config.

---
+++ rjt-custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (wombat)))
+ '(custom-safe-themes (quote ("3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" default)))
+ '(inhibit-startup-screen t)
+ '(show-paren-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "adobe" :slant normal :weight normal :height 100 :width normal)))))
---
+++ rjt.el
@@ -0,0 +1,70 @@
+;; Load el-get to manage the packages.
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+
+(require 'el-get)
+
+;; Packages that require local config.
+(setq el-get-sources
+      '((:name magit
+         :after (global-set-key (kbd "C-x C-z") 'magit-status))
+	
+	(:name autopair
+	 :after (autopair-global-mode))
+
+	(:name flycheck
+	 :after (global-flycheck-mode))
+
+	(:name auto-complete
+	 :after (add-hook 'python-mode-hook 'auto-complete-mode))
+
+	(:name jedi
+	 :after (add-hook 'python-mode-hook 'jedi:setup))
+	))
+
+
+(setq my-packages
+      (append
+       '(cl-lib  ;; Packages that do not require local config.
+	 autopep8
+	 ctable
+	 dash
+	 deferred
+	 el-get
+	 epc
+	 epl
+	 f
+	 fuzzy
+	 package
+	 pkg-info
+	 popup
+	 python-environment
+	 s)
+       (mapcar 'el-get-source-name el-get-sources)))
+
+(el-get-cleanup my-packages)
+(el-get 'sync my-packages)
+
+;; Prepackaged mode config.
+(show-paren-mode 1)
+(require 'ido) (ido-mode t)
+
+;; Python Mode
+(setq python-indent 4)
+(add-hook 'python-mode-hook
+          (function (lambda ()
+                      (setq indent-tabs-mode nil
+                            tab-width 4
+			    python-indent 4))))
+
+
+(setq custom-file "~/.emacs.d/rjt-custom.el")
+(load custom-file)
